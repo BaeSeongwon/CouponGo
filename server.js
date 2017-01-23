a660654b9d89f82549bb98b2a2132e62a6b1519a
성원 <seongwon9179@gmail.com>
2017-01-23 16:51:57 +0900

    mysql 모듈 업로드

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var http = require('http');
 var express = require('express');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'hue.c4yiq5vuopvi.ap-northeast-2.rds.amazonaws.com'
+  port: 3306,
+  user: 'root',
+  password: 'seongwon9179',
+  database: 'CouponGo',
+});
 
 var app = express(app);
 app.use(express.static(__dirname));
 
 http.createServer(app).listen(80,function(){
+  connection.connect(function(err){
+    if(err){
+      console.log("실패 ㅠㅠ");
+    }else{
+      console.log("DB 실행 완료");
+    }
+  })
   console.log("서버 실행 포트는 3000!!");
 });
 
