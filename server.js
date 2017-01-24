ce1993502e7a4a99edf45434c1765850ced496a3
성원 <seongwon9179@gmail.com>
2017-01-25 05:12:13 +0900

    no message

--- server.js
+++ server.js
@@ -157,12 +157,12 @@ app.post('/my_coupon' , function (req,res,next) {
                 connection.query(sql2,data[i].coupon_id,function (err,sucs)
                 {
                   console.log("sucs.co_image ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ");
-                  console.log(sucs.co_image);
-                    suc.push(sucs[i].co_image);
+                  console.log(sucs[0].co_image);
+
 
                     if(judge == data.length - 1){
 
-                        res.send(sucs[i].co_image);
+                        res.send(sucs[0].co_image);
                         connection.release();
                     }else{
                         judge++;
