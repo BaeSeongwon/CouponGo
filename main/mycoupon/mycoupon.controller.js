eeba6eda21564430c68904614ba2a3940f90ee22
성원 <seongwon9179@gmail.com>
2017-01-25 04:26:22 +0900

    no message

--- main/mycoupon/mycoupon.controller.js
+++ main/mycoupon/mycoupon.controller.js
@@ -20,7 +20,7 @@
     function getMyCoupon(){
       var data = couponService.getMyCoupon();
       myCoupon.datas = data;
-      console.log(data[0]);
+      console.log(data['Object']);
     }
   }
 })()
