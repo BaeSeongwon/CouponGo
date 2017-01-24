d8eb01a2ae70153e51b246516a9ffd5579de4e66
성원 <seongwon9179@gmail.com>
2017-01-24 15:58:35 +0900

    로그인 기능 v1.0.1

--- main/login/login.controller.js
+++ main/login/login.controller.js
@@ -9,6 +9,7 @@
   function loginCtrl(){
     var login = this;
     login.moveMain = moveMain;
+    login.beLogin = beLogin;
 
     function moveMain(){
       window.location.href = "http://ec2-52-78-41-172.ap-northeast-2.compute.amazonaws.com/#!";
