b716e0b398e7a5276b4e42693def0ce5c8131b8d
성원 <seongwon9179@gmail.com>
2017-01-25 03:40:11 +0900

    no message

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ var name= "parker";
       //             else{
       //                     judge++;
       //                   }
-      //                 });
+                      });
 
 
   }
